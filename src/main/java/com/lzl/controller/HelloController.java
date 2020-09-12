480fd341497a2ae26ce1d96553342cae14435718
刘哲良 <a1565086035@qq.com>
2020-09-12 20:14:00 +0800

    测试项目提交

--- src/main/java/com/lzl/controller/HelloController.java
+++ src/main/java/com/lzl/controller/HelloController.java
@@ -11,6 +11,7 @@ public class HelloController {
     @ResponseBody
     @RequestMapping("/hello")
     public String hello(){
+        String str = "helloWorld";
         return "hello world!!";
     }
 }
